62c196f681cf612dcbe9b8869a4f857e6f191226
fajradim <63057526+fajradim@users.noreply.github.com>
2022-10-06 00:23:54 +0700

    Add files via upload

---
+++ class.php
@@ -0,0 +1,20 @@
+<?php
+	class Product	
+	{
+		protected $name;
+		protected $price;
+		protected $discount;
+	
+		function_construct()
+		{	
+		echo "demoya $name";
+		}
+		
+		var_dump($name);
+		
+	}
+	
+	
+		
+	
+?>
